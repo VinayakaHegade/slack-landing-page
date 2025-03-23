58ecdb2d0d041fb046b97ac9e8b749ce0d4f5075
VinayakaHegade <myselfvinayakahegade@gmail.com>
2025-03-23 12:53:27 +0530

    chore: setup theming

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
 export default function Home() {
   return (
     <div>
-      <h1>Hello World</h1>
+      {/* Using utility classes */}
+      <h1 className="font-avant">This is Avant Garde</h1>
+      <p className="font-sans-regular">This is Sans Regular</p>
+
+      {/* These will automatically use the correct fonts based on the base layer */}
+      <h2>This heading automatically uses Avant Garde</h2>
+      <p>This paragraph automatically uses Sans Regular</p>
     </div>
   );
 }
